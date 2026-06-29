62a419236f0199c72600b7aef5265c156f81a84e
ThanhhhDat <thanhdatkbtb@gmail.com>
2026-06-29 12:08:52 +0700

    Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
 pcall(function()
-    local url = game.GameId == 10200395747 and "https://raw.githubusercontent.com/ThanhhhDat/ThanhhhDat/main/GaG2.lua" or "https://raw.githubusercontent.com/ThanhhhDat/ThanhhhDat/main/BloxFruits.lua"
+    local url = game.GameId == 97598239454123 and "https://raw.githubusercontent.com/ThanhhhDat/ThanhhhDat/main/GaG2.lua" or "https://raw.githubusercontent.com/ThanhhhDat/ThanhhhDat/main/BloxFruits.lua"
     loadstring(game:HttpGet(url))()
 end)
